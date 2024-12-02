2e711d6b729090316f7b323ce1547666a86ad138
PiotrekPKP <peterpkppeter@gmail.com>
2024-12-02 19:28:51 +0100

    fix: api

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import { View, Text } from "react-native";
 
 export default function HomeScreen() {
+  console.log("Hello from HomeScreen!", process.env.EXPO_PUBLIC_API_URL!);
+
+  useEffect(() => {
+    fetch(process.env.EXPO_PUBLIC_API_URL!)
+      .then((r) => r.json())
+      .then(console.log);
+  }, []);
+
   return (
     <View className="p-8">
       <Text className="text-xl">Index</Text>
